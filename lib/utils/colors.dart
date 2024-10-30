b83e564b9b6257d3f692bab3c2e606d0d720963b
youssef muhamed <youssef.mu.saber@gmail.com>
2024-10-30 19:58:26 +0300

    Initial commit

---
+++ lib/utils/colors.dart
@@ -0,0 +1,25 @@
+// This file contains all the colors that will be used in the whole application.
+
+import 'dart:ui';
+
+const Color finishTextColor = Color(0xff0087ff);
+const Color finishBackgroundColor = Color(0xffe3f2ff);
+
+const Color inprogressTextColor = Color(0xff5f33e1);
+const Color inprogressBackgroundColor = Color(0xfff0ecff);
+
+const Color waitingTextColor = Color(0xffff7d53);
+const Color waitingBackgroundColor = Color(0xffffe4f2);
+
+const Color errorTextColor = Color(0xfff32828);
+const Color errorBackgroundColor = Color(0xffffe4e4);
+
+const Color successTextColor = Color(0xff59e133);
+const Color successBackgroundColor = Color(0xffe6ffe4);
+
+const Color whiteBackground = Color(0xffffffff);
+
+const Color darkTextColor = Color(0xff24252c);
+const Color lightTextColor = Color(0x9924252c);
+
+const Color lightGrayBorder = Color (0xffbababa);
